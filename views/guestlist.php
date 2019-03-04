117e3ce45aac19f06cde2d1f1f4d301ac8fdcbde
Chris Smith <chris@cgsmith.net>
2019-03-03 20:26:53 -0600

    Guest list view

---
+++ views/guestlist.php
@@ -0,0 +1,48 @@
+
+<div class="row">
+    <div class="col-md-12 order-md-1">
+        <h3 class="mb-3">Your ticket information</h3>
+        <h5 class="mb-3">Guest #1 <small>(Table #1, Paddle #102)</small></h5>
+        <form class="needs-validation" novalidate>
+            <div class="row">
+                <div class="col-md-2 mb-3">
+                    <label for="firstName">Full name</label>
+                    <input type="text" class="form-control" id="firstName" value="">
+                </div>
+                <div class="col-md-2 mb-3">
+                    <label for="lastName">Email</label>
+                    <input type="text" class="form-control" id="email" value="">
+                </div>
+                <div class="col-md-2 mb-3">
+                    <label for="lastName">Phone</label>
+                    <input type="text" class="form-control" id="phone" value="">
+                </div>
+                <div class="col-md-2 mb-3">
+                    <label for="lastName">Childcare?</label>
+                    <select class="form-control" name="childcare">
+                        <option>No</option>
+                        <option>Yes</option>
+                    </select>
+                </div>
+                <div class="col-md-2 mb-3">
+                    <label for="lastName">Valet?</label>
+                    <select class="form-control" name="valet">
+                        <option>No</option>
+                        <option>Yes</option>
+                    </select>
+                </div>
+                <div class="col-md-2 mb-3">
+                    <label for="lastName">Restrictions?</label>
+                    <select class="form-control" name="restrictions">
+                        <option>None</option>
+                        <option>Vegetarian</option>
+                        <option>Vegan</option>
+                    </select>
+                </div>
+            </div>
+
+            <hr class="mb-4">
+            <button class="btn btn-primary btn-lg btn-block" type="submit">Save</button>
+        </form>
+    </div>
+</div>

a27abb22c144ad238d9f2410aa9b01b39de84602
Chris Smith <chris@cgsmith.net>
2019-03-08 11:23:26 -0600

    Link to main site

--- views/common/head.php
+++ views/common/head.php
@@ -55,8 +55,11 @@
 
     <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+            <li class="nav-item">
+                <a class="nav-link" href="/">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="https://discovernci.org">Learn more about Nature's Classroom</a>
             </li>
         </ul>
     </div>

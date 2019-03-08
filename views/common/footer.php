7754e3e804e09ef1b520ac0cb5685ed95dfceaaf
Chris Smith <chris@cgsmith.net>
2019-03-08 12:06:54 -0600

    footer copyright

--- views/common/footer.php
+++ views/common/footer.php
@@ -3,7 +3,7 @@
 <footer class="container pt-2 text-muted text-center text-small">
     <div class="row">
         <div class="top-20 col-md-12">
-            <p class="mb-1">&copy; <?= date('Y'); ?> Natures Classroom Institute of Wisconsin</p>
+            <p class="mb-1">&copy; <?= date('Y'); ?> Natures Classroom Institute of Wisconsin, All rights reserved.</p>
             <p class="center">Learning through experience. Growing through expression.<a href="https://www.facebook.com/NaturesClassroomInstitute" style="margin-left:15px;color:#5bb3ff" rel="noopener noreferrer" target="_blank"><span class="fa fa-thumbs-o-up fa-fw" aria-hidden="true"></span> Like us on Facebook</a> <a href="https://www.instagram.com/naturesclassroominstitute/" style="margin-left:15px;color:#5bb3ff" rel="noopener noreferrer" target="_blank"><span class="fa fa-instagram fa-fw" aria-hidden="true"></span> Follow us on Instagram</a></p>
         </div>
     </div>

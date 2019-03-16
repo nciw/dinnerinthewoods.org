eb3d3d44c085e573b934e122d37c58f646be91ab
Chris Smith <chris@cgsmith.net>
2019-03-15 20:05:04 -0500

    Admin page

---
+++ views/admin.php
@@ -0,0 +1,55 @@
+<div class="row">
+    <div class="col-md-12 order-md-1">
+        <h3 class="mb-3">Orders <a href="/admin/order/export" class="btn btn-primary">Export</a></h3>
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th scope="col">Order #</th>
+                <th scope="col">Name</th>
+                <th scope="col">Email</th>
+                <th scope="col">Tickets</th>
+                <th scope="col">Enhancers</th>
+                <th scope="col">Total</th>
+                <th scope="col"></th>
+                <th scope="col"></th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php foreach ($orders as $order) { ?>
+                <tr>
+                    <th scope="row"><?= $order->id; ?></th>
+                    <td><?= $order->first_name . ' ' . $order->last_name; ?></td>
+                    <td><a href="mailto:<?=$order->email?>"><?= $order->email ?></a></td>
+                    <td><?=$order->ticket_quantity;?></td>
+                    <td><?=$order->enhancer_quantity;?></td>
+                    <td>
+                        <?php // Stripe payment
+                        if (!$order->payment_type) { ?>
+                            <a target="_blank" href="https://dashboard.stripe.com/payments/<?=$order->stripe_token?>"><?='$' . number_format(($order->total_cents / 100), 2);?></a>
+                        <?php }else {
+                         echo '$' . number_format(($order->total_cents / 100), 2);
+                        } ?>
+                    </td>
+                    <td style="text-align: center;">
+                        <?php
+                        if ($order->cabana_cents > 0) {
+                            echo '<span class="badge badge-secondary">Cabana</span><br/>';
+                        }
+                        if ($order->additional_cents > 0) {
+                            echo '<span class="badge badge-info">$ '.number_format(($order->additional_cents / 100), 2) .'</span><br/>';
+                        }
+                        if ($order->payment_type) {
+                            echo '<span class="badge badge-warning">Check</span>';
+                        }
+                        ?>
+                    </td>
+                    <td>
+                        <a href="/admin/order/<?=$order->id?>" class="btn btn-primary" role="button">Order</a>
+                    </td>
+                </tr>
+            <?php } ?>
+            </tbody>
+        </table>
+        <br/>
+    </div>
+</div>

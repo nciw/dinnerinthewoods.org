7782332b14c5148a4eec91ba7e5dfa23ca3696ee
Chris Smith <chris@cgsmith.net>
2019-05-01 14:21:56 -0500

    4 weeks

--- views/blog.php
+++ views/blog.php
@@ -2,7 +2,7 @@
 <div class="row">
     <div class="col-md-12 mb-12 text-center">
         <h3>News and Updates</h3>
-        <a href="/assets/dinner-in-the-woods-is-only-5-weeks-away.pdf">Dinner in the Woods is Only 5 Weeks Away!</a>
+        <a href="/assets/dinner-in-the-woods-is-only-5-weeks-away.pdf">Dinner in the Woods is Only 4 Weeks Away!</a>
     </div>
     <br/>
     <br/>

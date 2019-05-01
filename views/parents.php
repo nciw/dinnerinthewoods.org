ce6dca3ba70c226ee7e2f25b097ce76f7506e17c
Chris Smith <chris@cgsmith.net>
2019-04-30 20:05:18 -0500

    parent link and asset

--- views/parents.php
+++ views/parents.php
@@ -1,11 +1,10 @@
 
 <div class="row">
     <div class="col-md-12 mb-12 text-center">
-        <h3></h3>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
+        <h3>DITW Parent Information</h3>
+        <a href="/assets/ditw-committees.xlsx">Committee Information</a><br/>
+        <a href="https://www.signupgenius.com/go/10c0548a9ae2fa7f94-dinner">Silent Auction Baskets</a><br/>
+        <a href="https://docs.google.com/spreadsheets/d/1y7rQroRYCGhOZkujjj9SnqJ2DwduyBRHwjZSe8BFCVc/edit#gid=0">Contacts for Donations</a><br/>
     </div>
 
 </div>
